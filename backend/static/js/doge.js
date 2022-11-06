76b99e51a5b52d1ae95bb94d9234392bed10bfdf
Konrad/p <konrad@eichsfeld.net>
2022-11-06 14:03:21 +0100

    last changes

--- backend/static/js/doge.js
+++ backend/static/js/doge.js
@@ -10,3 +10,93 @@ var map = new ol.Map({
     zoom: 2
   })
 });
+
+
+function onCountryMouseOut(e){
+	C.geojson.resetStyle(e.target);
+//	$("#countryHighlighted").text("No selection");
+
+	var countryName = e.target.feature.properties.name;
+	var countryCode = e.target.feature.properties.iso_a2;
+//callback when mouse exits a country polygon goes here, for additional actions
+}
+
+/**
+ * Callback for when a country is clicked. Will take care of the ui aspects, and it will call
+ * other callbacks when done
+ * @param e
+ */
+function onCountryClick(e){
+//callback for clicking inside a polygon
+}
+
+/**
+ * Callback for when a country is highlighted. Will take care of the ui aspects, and it will call
+ * other callbacks after done.
+ * @param e
+ */
+function onCountryHighLight(e){
+	var layer = e.target;
+
+	layer.setStyle({
+		weight: 2,
+		color: '#666',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera) {
+		layer.bringToFront();
+	}
+
+	var countryName = e.target.feature.properties.name;
+	var countryCode = e.target.feature.properties.iso_a2;
+//callback when mouse enters a country polygon goes here, for additional actions
+}
+
+/**
+ * Callback for mouse out of the country border. Will take care of the ui aspects, and will call
+ * other callbacks after done.
+ * @param e the event
+ */
+ function onCountryMouseOut(e){
+	C.geojson.resetStyle(e.target);
+//	$("#countryHighlighted").text("No selection");
+
+	var countryName = e.target.feature.properties.name;
+	var countryCode = e.target.feature.properties.iso_a2;
+//callback when mouse exits a country polygon goes here, for additional actions
+}
+
+/**
+ * Callback for when a country is clicked. Will take care of the ui aspects, and it will call
+ * other callbacks when done
+ * @param e
+ */
+function onCountryClick(e){
+//callback for clicking inside a polygon
+}
+
+/**
+ * Callback for when a country is highlighted. Will take care of the ui aspects, and it will call
+ * other callbacks after done.
+ * @param e
+ */
+function onCountryHighLight(e){
+	var layer = e.target;
+
+	layer.setStyle({
+		weight: 2,
+		color: '#666',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera) {
+		layer.bringToFront();
+	}
+
+	var countryName = e.target.feature.properties.name;
+	var countryCode = e.target.feature.properties.iso_a2;
+//callback when mouse enters a country polygon goes here, for additional actions
+}
